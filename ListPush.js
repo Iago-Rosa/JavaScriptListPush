097ae1ec3d9ae50dca42ef7973b77294ab9cd65b
Iago-Rosa <iagorosa045@gmail.com>
2022-04-12 20:19:22 -0300

    A rquivo

---
+++ ListPush.js
@@ -0,0 +1,18 @@
+
+//Criando uma lista vazia e inserindo dados e lendo a lista
+var frutas = [];
+frutas.push("Pessego")
+frutas.push("Manga")
+frutas.push("Maça")
+frutas.push("Laranja")
+frutas.push("limão")
+frutas.push("Abacate")
+
+
+document.write(frutas)
+console.log(frutas)
+
+
+
+
+
